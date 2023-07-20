4391caaa1a931ae204ec8e37f3b867f6df7bcf71
Richielly <richielly.positivo@gmail.com>
2023-07-20 10:34:39 -0300

    V_0.0.1

--- arquivos.py
+++ arquivos.py
@@ -13,13 +13,20 @@ def descompactar_arquivos(diretorio_raiz):
         for file in filenames:
             if file.endswith(('.zip', '.rar', '.7z')):
                 arquivo_completo = os.path.join(dirpath, file)
-                # Define o diretório de saída como nome do arquivo sem a extensão
-                diretorio_saida = os.path.join(dirpath, os.path.splitext(file)[0])
 
-                # Se o diretório de saída não existir, crie-o
-                if not os.path.exists(diretorio_saida):
-                    os.makedirs(diretorio_saida)
+                # Se for um arquivo .rar, extrai no mesmo diretório
+                if file.endswith('.rar'):
+                    diretorio_saida = dirpath
+                else:
+                    # Define o diretório de saída como nome do arquivo sem a extensão
+                    diretorio_saida = os.path.join(dirpath, os.path.splitext(file)[0])
 
+                    # Se o diretório de saída já existir, avise e continue para o próximo arquivo
+                    if os.path.exists(diretorio_saida):
+                        print(f"Diretório '{diretorio_saida}' já existe. Pulando descompactação de {file}.")
+                        continue
+
+                # Descompacta o arquivo
                 try:
                     print(f"Descompactando: {arquivo_completo}")
                     patoolib.extract_archive(arquivo_completo, outdir=diretorio_saida)
